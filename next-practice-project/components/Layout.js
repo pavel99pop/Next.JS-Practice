35bba605755d498491eba30961ff3365eda99ea3
pavel99pop <pavel99pop@gmail.com>
2021-06-27 16:57:34 +0600

    Custom header for each page and more SEO friendly Meta component and finishing touches

--- next-practice-project/components/Layout.js
+++ next-practice-project/components/Layout.js
@@ -1,6 +1,6 @@
 import Meta from "./Meta";
-import Header from "./Header";
 import Nav from "./Nav";
+import Header from "./Header";
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
--- next-practice-project/components/Meta.js
+++ next-practice-project/components/Meta.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
       <meta charSet="utf-8" />
